524ed09b3783f9e311c4d6a24a77da7831338606
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2024-08-15 20:22:31 +0530

    new Server URL

--- frontend/src/setup.js
+++ frontend/src/setup.js
@@ -1,3 +1,3 @@
-const SERVER_URL = "https://quickbite-backend-r4mlunp2sa-el.a.run.app"  // GCP Cloud Run
+const SERVER_URL = "https://quickbite-cms-r4mlunp2sa-el.a.run.app/"  // GCP Cloud Run
 // const SERVER_URL = "http://localhost:5000"  // Localhost
 export {SERVER_URL}
