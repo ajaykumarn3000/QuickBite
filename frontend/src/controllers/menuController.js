8bbaab3ca3bca93747b956ad95f47abda3978881
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2024-08-15 19:30:11 +0530

    Console.log added

--- frontend/src/controllers/menuController.js
+++ frontend/src/controllers/menuController.js
@@ -2,12 +2,14 @@ import { SERVER_URL } from "../setup";
 
 const getMenu = async (token) => {
   try {
+    console.log("Menu URL: " + SERVER_URL + "/kitchen/menu");
     const res = await fetch(SERVER_URL + "/kitchen/menu", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
     const data = await res.json();
+    console.log("SUCCESS");
     if (res.ok) {
       data.forEach(element => {
         element.selected = false;
