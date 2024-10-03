e77514457d6e227587433e14c3eb7e2259df25cf
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2024-10-03 13:42:42 +0530

    Payment Gateway

---
+++ frontend/src/components/Admin/Dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='ml-0 m-2 bg-white rounded shadow grow'>Dashboard</div>
+  )
+}
+
+export default Dashboard
---
+++ frontend/src/components/Admin/ManageMenu/ManageMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MenuCard from './MenuCard'
+
+const ManageMenu = () => {
+  return (
+    <div className='m-2 ml-0 bg-white shadow rounded grow'>
+      <MenuCard  />
+    </div>
+  )
+}
+
+export default ManageMenu
