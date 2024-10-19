d95973cca9af807bab9926f9fa729aa32e03557c
KXN2004 <kevinxaviernadar@student.sfit.ac.in>
2024-10-20 03:13:03 +0530

    Added v1 prefix to routes

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,40 @@
 # -*- coding: utf-8 -*-
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
+from routes.kitchen.menu_card import router as menu_card_router
 from routes.user.api import router as user_api_router
 from routes.user.auth import router as user_auth_router
-from routes.kitchen.menu_card import router as menu_card_router
 
 app = FastAPI()
 
-origins = ["*"]
+version = "v2"
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-@app.get("/")
+@app.get(f"/{version}")
 def root():
-    return {"message": "Connected to QuickBite CMS"}
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content="Connected to QuickBite CMS Backend",
+    )
 
 
-app.include_router(router=user_auth_router)
-app.include_router(router=user_api_router)
-app.include_router(router=menu_card_router)
+# Create a global API router with the /{version} prefix
+api_router = APIRouter(prefix=f"/{version}")
+
+# Include all individual routers under this main API router
+api_router.include_router(user_auth_router)
+api_router.include_router(user_api_router)
+api_router.include_router(menu_card_router)
+
+# Include the API router in the FastAPI app
+app.include_router(api_router)
