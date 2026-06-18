0e52b3bc95fbb81eaba2ad202c5b087278be0d0a
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2026-06-19 00:36:22 +0530

    fix: update SERVER_URL to include version in path

--- admin/src/setup.js
+++ admin/src/setup.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = process.env.REACT_APP_PUBLIC_BACKEND_URL || process.env.PUBLIC_BACKEND_URL || "https://quickbite-sfit.vercel.app/_/backend";
+export const SERVER_URL = process.env.REACT_APP_PUBLIC_BACKEND_URL || process.env.PUBLIC_BACKEND_URL || "https://quickbite-sfit.vercel.app/_/backend/v1";
 // export const SERVER_URL = "https://quickbite.ddns.net/v1";
 export const FOOD_TYPES = [
   "Breakfast",
--- frontend/src/setup.js
+++ frontend/src/setup.js
@@ -1,2 +1,2 @@
-const SERVER_URL = process.env.REACT_APP_PUBLIC_BACKEND_URL || process.env.PUBLIC_BACKEND_URL || "https://quickbite-sfit.vercel.app/_/backend";
+const SERVER_URL = process.env.REACT_APP_PUBLIC_BACKEND_URL || process.env.PUBLIC_BACKEND_URL || "https://quickbite-sfit.vercel.app/_/backend/v1";
 export { SERVER_URL };
